5f2704599b0466dbc1cd688d266c3db2656944ea
keerthipalliboyina <147791828+keerthipalliboyina@users.noreply.github.com>
2025-09-18 20:16:41 +0530

    Time: 12 ms (97.53%), Space: 46.6 MB (78.06%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max=0;
+        for(int i=0;i<nums.length;i=i+2)
+        {
+            max=max+nums[i];
+        }
+        return max;
+    }
+}
