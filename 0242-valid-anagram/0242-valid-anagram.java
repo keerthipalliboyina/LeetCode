1f28b9364c0555090ac7725c22f674fc0d990da7
keerthipalliboyina <147791828+keerthipalliboyina@users.noreply.github.com>
2026-08-03 14:57:14 +0530

    Time: 16 ms (20.77%), Space: 46.6 MB (25.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,28 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-         HashMap<Character,Integer> fre=new HashMap<>();
+        int n1=s.length();
         int n2=t.length();
-         int n1=s.length();
-         char[] arr=s.toCharArray();
-         char[] arr2=t.toCharArray();
-        
-         for(int i=0;i<n1;i++)
-         {
-            fre.put(arr[i],fre.getOrDefault(arr[i],0)+1);
-         }
-         for(int i=0;i<n2;i++)
-         {
-            if(!fre.containsKey(arr2[i]))
-            {
-                 return false;
-            }
-            fre.put(arr2[i],fre.get(arr2[i])-1);
-             if(fre.get(arr2[i]) < 0){
-                return false;
-             }
+         if (s.length() != t.length()) {
+            return false;
+        }
+        char[] a1=s.toCharArray();
+        char[] a2=t.toCharArray();
 
-         }
-         return true;
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(int i=0;i<n1;i++)
+        {
+             map.put(a1[i],map.getOrDefault(a1[i],0)+1);
+        }
+
+        for(int j=0;j<n2;j++)
+        {
+            if(!map.containsKey(a2[j]))
+            {
+                return false;
+            }
+
+            map.put(a2[j], map.get(a2[j]) - 1);
+            if(map.get(a2[j])<0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
